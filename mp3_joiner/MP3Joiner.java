fff2850db0a0ae2445590cc3a2fc7e86d7610bcb
Daniel Koics <koics.dani@gmail.com>
2016-02-18 16:28:27 +0100

    Joiner method implemented but tests needed, problems with destination path

--- mp3_joiner/MP3Joiner.java
+++ mp3_joiner/MP3Joiner.java
@@ -7,6 +7,8 @@ import java.io.RandomAccessFile;
 import java.util.List;
 
 public class MP3Joiner {
+	
+	private static final int SIZE_OF_BUFFER = (1 << 20);
 
 	// checker used for mp3 files:
 	static boolean checkIfValidMP3(File mp3File) {
@@ -18,7 +20,49 @@ public class MP3Joiner {
 
 	// joiner function:
 	static void join(List<File> sourceFileList, File destinationFile) throws IOException {
-		
+		RandomAccessFile writer = new RandomAccessFile(destinationFile, "rws");
+		for (File sourceFile: sourceFileList) {
+			append(sourceFile, writer);
+		}
+		writer.close();
+	}
+	
+	private static void append(File sourceFile, RandomAccessFile writerOfDestinationFile) throws IOException {
+		long lengthOfDestinationFile = writerOfDestinationFile.length();
+		if (lengthOfDestinationFile > 128 && checkIfID3Tagged(writerOfDestinationFile)) {
+			writerOfDestinationFile.setLength(lengthOfDestinationFile - 128);
+			lengthOfDestinationFile -= 128;
+		}
+		writerOfDestinationFile.seek(lengthOfDestinationFile);
+		RandomAccessFile readerOfSourceFile = new RandomAccessFile(sourceFile, "r");
+		long lengthOfSourceFile = readerOfSourceFile.length();
+		readerOfSourceFile.seek(0);
+		{
+			long positionInDestinationFile = lengthOfDestinationFile;
+			long positionInSourceFile = 0;
+			byte[] bytesToCopy = new byte[SIZE_OF_BUFFER];
+			int numberOfBytesRead;
+			while (positionInSourceFile < lengthOfSourceFile) {
+				numberOfBytesRead = readerOfSourceFile.read(bytesToCopy, 0, SIZE_OF_BUFFER);
+				positionInSourceFile += numberOfBytesRead;
+				writerOfDestinationFile.write(bytesToCopy, 0, numberOfBytesRead);
+				positionInDestinationFile += numberOfBytesRead;
+			}
+		}
+		readerOfSourceFile.close();
+	}
+	
+	private static boolean checkIfID3Tagged(RandomAccessFile writerOfFile) throws IOException {
+		return getBeginningOfTail(writerOfFile).equals("TAG");
+	}
+	
+	private static String getBeginningOfTail(RandomAccessFile writerOfFile) throws IOException {
+		long currentPosition = writerOfFile.getFilePointer();
+		writerOfFile.seek(writerOfFile.length() - 128);
+		byte[] bytes = new byte[3];
+		writerOfFile.read(bytes, 0, 3);
+		writerOfFile.seek(currentPosition);
+		return new String(bytes);
 	}
 	
 }

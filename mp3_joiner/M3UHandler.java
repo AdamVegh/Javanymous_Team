68c79db01dfb0ca9631307ddb26e97c053033bec
Daniel Koics <koics.dani@gmail.com>
2016-02-18 18:58:01 +0100

    format change in mp3_joiner.M3UHandler

--- mp3_joiner/M3UHandler.java
+++ mp3_joiner/M3UHandler.java
@@ -10,8 +10,8 @@ public class M3UHandler {
 
 	// main method:
 	public static File joinMP3FilesFromM3U(File m3uFile) throws IOException {
-			M3UFile m3uData = new M3UFile(m3uFile);
-			return m3uData.joinMP3Files();
+		M3UFile m3uData = new M3UFile(m3uFile);
+		return m3uData.joinMP3Files();
 	}
 	
 	// another accessory:

c0897d75c46b0177489bf27927f0aeb6909691d2
Vegh Adam <veghadam1991@gmail.com>
2016-02-18 20:12:51 +0100

    fix YearSorter

--- sorter/YearSorter.java
+++ sorter/YearSorter.java
@@ -17,9 +17,9 @@ public class YearSorter extends Sorter{
 		Id3TagMp3 tag = Id3TagMp3.parse(o1);
 		Id3TagMp3 tag2 = Id3TagMp3.parse(o2);
 		if (ascending) {
-			return tag.getYear() < tag2.getYear() ? -1 : tag.getYear() == tag2.getYear() ? 0 : 1;
+			return Integer.parseInt(tag.getYear()) < Integer.parseInt(tag2.getYear()) ? -1 : tag.getYear().equals(tag2.getYear()) ? 0 : 1;
 		}
-		return tag.getYear() < tag2.getYear() ? 1 : tag.getYear() == tag2.getYear() ? 0 : -1;
+		return Integer.parseInt(tag.getYear()) < Integer.parseInt(tag2.getYear()) ? 1 : tag.getYear().equals(tag2.getYear()) ? 0 : -1;
 
 	}
 }

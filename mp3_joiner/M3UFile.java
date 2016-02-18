2965c57c90bf2a1a2772f63a9373c597c5ca807a
Daniel Koics <koics.dani@gmail.com>
2016-02-18 17:56:20 +0100

    refactored mp3_joiner.M3UFile

--- mp3_joiner/M3UFile.java
+++ mp3_joiner/M3UFile.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.plaf.ActionMapUIResource;
-
 public class M3UFile {
 
 	// member variables:
@@ -20,28 +18,15 @@ public class M3UFile {
 	M3UFile(File m3uFile) throws IOException {
 		if (!checkIfValidM3U(m3uFile))
 			throw new IOException("Inappropriate m3u-file");
-		String m3uName = m3uFile.getAbsolutePath();
-		this.m3uFile = m3uFile;
-		String destinationName = m3uName.substring(0, m3uName.length() - ".m3u".length()) + ".mp3"; 
-		this.destinationFile = new File(destinationName);
-		BufferedReader reader = new BufferedReader(new FileReader(m3uFile));
-		this.sourceFileList = new ArrayList<File>();
-		{
-			String line = null;
-			String nonComment = null;
-			File actualSourceFile = null;
-			while ((line = reader.readLine()) != null) {
-				if (line.startsWith("#"))
-					continue;
-				nonComment = line.split("#")[0];
-				actualSourceFile = new File(nonComment);
-				if (! MP3Joiner.checkIfValidMP3(actualSourceFile))
-					throw new IOException("Wrong file reference in m3u file");
-				this.sourceFileList.add(actualSourceFile);
-			}
-		}	
-		reader.close();
+		this.m3uFile = m3uFile; 
+		this.destinationFile = createDestinationFileObj(m3uFile);
+		this.sourceFileList = parseM3UFile(m3uFile);
 	}
+	
+	// getters:
+	File getM3UFile() { return m3uFile; }
+	File getDestinationFile() { return destinationFile; }
+	List<File> getSourceFileList() { return sourceFileList; }
 	
 	// m3u file-checker used by constructor:
 	static boolean checkIfValidM3U(File m3uFile) {
@@ -55,6 +40,34 @@ public class M3UFile {
 	File process() throws IOException {
 		MP3Joiner.join(sourceFileList, destinationFile);
 		return destinationFile;
+	}
+	
+	// other assistant functions used by constructor:
+	private static File createDestinationFileObj(File m3uFile) {
+		String m3uPath = m3uFile.getAbsolutePath();
+		String destinationPath = m3uPath.substring(0, m3uPath.length() - ".m3u".length()) + ".mp3";
+		return new File(destinationPath);
+	}
+	
+	private static List<File> parseM3UFile(File m3uFile) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(m3uFile));
+		List<File> sourceFileList = new ArrayList<>();
+		{
+			String line = null;
+			String nonComment = null;
+			File actualSourceFile = null;
+			while ((line = reader.readLine()) != null) {
+				if (line.startsWith("#"))
+					continue;
+				nonComment = line.split("#")[0];
+				actualSourceFile = new File(nonComment);
+				if (! MP3Joiner.checkIfValidMP3(actualSourceFile))
+					throw new IOException("Wrong file reference in m3u file");
+				sourceFileList.add(actualSourceFile);
+			}
+		}
+		reader.close();
+		return sourceFileList;
 	}
 	
 }

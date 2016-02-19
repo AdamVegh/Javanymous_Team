e54cc08fc3b6c9bd75dd75b24a44f3da1e2a3151
Szabó Máté <larde15@gmail.com>
2016-02-19 10:55:14 +0100

    update

--- menu/ModifySubmenu.java
+++ menu/ModifySubmenu.java
@@ -1,11 +1,19 @@
 package menu;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import exceptions.PossibleExceptions;
+import id3tag.Id3TagMp3;
+import userinput.UserInput;
 
 public class ModifySubmenu
 {
+	static Id3TagMp3 id3tag;
+	static String filePath;
+	
+	
+	
 	public void printMenu()
 	{
 //			Runtime.getRuntime().exec("cls");
@@ -24,44 +32,57 @@ public class ModifySubmenu
 	
 	
 	public void selectableMenuPoints()
-	{
-		String menuPoint = userInput();
+	{	
+		getLocationAndSetId3Tag();
+		int menuPoint = userInput();
 		MainMenu mM = new MainMenu();
-		SaveTagmenu doYouWantToChange = new SaveTagmenu();
+		SaveTagMenu doYouWantToChange = new SaveTagMenu();
 		
 		switch (menuPoint)
 		{
-		case "1":
+		case 1:
 			System.out.println("Title submenu");
+			String newTag = userInputNewData();
+			id3tag.setTitle(newTag);
 			doYouWantToChange.printMenu();
 			doYouWantToChange.selectableMenuPoints();
 			break;
-		case "2":
+		case 2:
 			System.out.println("Artist submenu");
+			String newTag2 = userInputNewData();
+			id3tag.setArtist(newTag2);
 			doYouWantToChange.printMenu();
 			doYouWantToChange.selectableMenuPoints();
 			break;
-		case "3":
+		case 3:
 			System.out.println("Album submenu");
+			String newTag3 = userInputNewData();
+			id3tag.setAlbum(newTag3);
 			doYouWantToChange.printMenu();
 			doYouWantToChange.selectableMenuPoints();
 			break;
-		case "4":
+		case 4:
 			System.out.println("Year submenu");
+			String newTag4 = userInputNewData();
+			id3tag.setYear(newTag4);
 			doYouWantToChange.printMenu();
 			doYouWantToChange.selectableMenuPoints();
 			break;
-		case "5":
+		case 5:
 			System.out.println("Comment submenu");
+			String newTag5 = userInputNewData();
+			id3tag.setComment(newTag5);
 			doYouWantToChange.printMenu();
 			doYouWantToChange.selectableMenuPoints();
 			break;
-		case "6":
+		case 6:
 			System.out.println("Genre submenu");
+			String newTag6 = userInputNewData();
+			id3tag.setGenre(newTag6);
 			doYouWantToChange.printMenu();
 			doYouWantToChange.selectableMenuPoints();
 			break;
-		case "7":
+		case 7:
 			mM.printMenu();
 			mM.selectableMenuPoints();
 			break;
@@ -73,11 +94,37 @@ public class ModifySubmenu
 		}
 	}
 	
-	public String userInput()
+	public int userInput()
 	{
 		BufferedReader bufferRead;
 		System.out.println();
     	System.out.print("Choose: ");
+		System.out.println();
+	    String s = "";
+	    
+		try
+		{
+			bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			s = bufferRead.readLine();
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.toString());
+		}
+	      
+		return Integer.parseInt(s);
+	}
+	
+	public void getLocationAndSetId3Tag(){
+		filePath = UserInput.getInputFromUser();
+		id3tag = Id3TagMp3.parse(new File(filePath));
+	}
+	
+	public String userInputNewData()
+	{
+		BufferedReader bufferRead;
+		System.out.println();
+    	System.out.print("NewTag: ");
 		System.out.println();
 	    String s = "";
 	    

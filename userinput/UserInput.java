0c20b36e68fc05b4c93e86bf7f8f59025bcf722c
Vegh Adam <veghadam1991@gmail.com>
2016-02-19 11:16:00 +0100

    fix in userinput

--- userinput/UserInput.java
+++ userinput/UserInput.java
@@ -3,6 +3,8 @@ package userinput;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+import sorter.SorterMenu;
 
 public class UserInput
 {
@@ -22,6 +24,43 @@ public class UserInput
 		}
         return input;
 	}
+	
+	public static String propertyChecker(String property) {
+		for (String string : SorterMenu.listProperty) {
+			if (property.equals(string)) return property;
+		}
+		System.out.println("This property doesn't exist.");
+		return "";
+	}
+	
+	public static String getPropertyInput() {
+		String propertyStr = "";
+		boolean propertyOK = false;
+		while (!propertyOK) {
+			System.out.print("Please type in the property: ");
+			for (String string : SorterMenu.listProperty) {
+				System.out.print(" | " + string);
+			}
+			System.out.println(" | ");
+			Scanner input = new Scanner(System.in);
+			propertyStr = propertyChecker(input.nextLine().toLowerCase());
+			propertyOK = (propertyStr != "");
+		}
+		return propertyStr;
+	}
+	
+	
+	public static boolean getAscendingInput() {
+		String ascendingStr = "";
+		boolean ascendingOK = false;
+		while (!ascendingOK) {
+			System.out.println("Direction of soring: [A]scending or [D]escending");
+			Scanner input = new Scanner(System.in);
+			ascendingStr = input.nextLine().toLowerCase();
+			ascendingOK = (ascendingStr.equals("a") || ascendingStr.equals("d"));
+		}
+		return ascendingStr.equals("a") ? true : false; 
+	};
 	
 	
 }
